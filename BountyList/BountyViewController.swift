8714c4946ca4ba4d6797a412f49f9e0353157ed9
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-21 21:29:55 +0900

    테이블뷰 기초 개념

---
+++ BountyList/BountyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by 류지영 on 2021/11/21.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
